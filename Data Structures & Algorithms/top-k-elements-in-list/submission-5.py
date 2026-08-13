c532573e2e7d997f593d96fd98cc9b8b20a697c3
Gyuwon Jung <152982748+Q1-in-Dublin@users.noreply.github.com>
2026-08-13 18:09:25 +0100

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,20 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #Solving with heap without most_common k
+        
+        count = Counter(nums)
+
+        #Frequency basket (make it more than the number of nums)
+        freq = [[] for _ in range(len(nums)+1)]
+        # [[], [], [], [], [], [], []]
+        for num, count in count.items():
+            freq[count].append(num)
+            # [[], [1], [2], [3], [], [], []]
+        result = []
+        #most frequent (start, stop, step):
+        for i in range(len(freq)-1, 0, -1):
+            for num in freq[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
