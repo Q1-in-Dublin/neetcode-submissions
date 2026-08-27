b3ea012fadfbacb61925533a1a0c584c07ddb3aa
Gyuwon Jung <152982748+Q1-in-Dublin@users.noreply.github.com>
2026-08-27 21:52:22 +0100

    Add: add-two-numbers - submission-0

---
+++ Data Structures & Algorithms/add-two-numbers/submission-0.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        #내가 첨에 생각했을때 한노드씩 더해서 만들라고했는데
+        #지금 규칙을보니까 맨끝으로가서 여기서서 숫자를 만드는거지
+        # 1->2->3 인데 321
+        # 4->5->6 인데 이게 654
+        # 결국 더하는건 321 + 654니까
+        #일단 더하고 뒤집기 같은데
+        # Carry
+
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+        #1,4
+        while carry or l1 or l2 :
+            val1 = l1.val if l1 else 0 #1
+            val2 = l2.val if l2 else 0 #4
+
+            # calculate carry
+            total= val1+val2+carry #5
+            carry = total // 10 #0
+            node_val = total % 10 # reaminers 5
+
+            #None->5
+            curr.next = ListNode(node_val)
+            #None->5(pointer here)
+            curr = curr.next
+
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+        return dummy.next
