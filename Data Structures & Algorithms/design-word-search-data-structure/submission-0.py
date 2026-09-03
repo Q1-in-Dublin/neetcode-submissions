65729af7d3c4899b443a8dbe019f1176a43ea653
Gyuwon Jung <152982748+Q1-in-Dublin@users.noreply.github.com>
2026-09-03 14:01:27 +0100

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,42 @@
+class TrieNode:
+
+    def __init__(self):
+            self.children ={}
+            self.is_end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+
+        for char in word : 
+            if char not in node.children :
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        # new thing . 
+        #. machtes anything
+        def dfs(node,i):
+            #index of the word
+            if i == len(word):
+                return node.is_end
+
+            char = word[i]
+
+            if char =='.':
+                for child_node in node.children.values():
+                    if dfs(child_node, i+1):
+                        return True
+                return False
+            else:
+                if char not in node.children:
+                    return False
+                return dfs(node.children[char], i + 1)
+
+        return dfs(self.root,0)
