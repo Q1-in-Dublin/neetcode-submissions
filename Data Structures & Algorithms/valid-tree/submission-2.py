01e54137c6350b2b7de69864a5c5e42c8c2dc5fe
Jacob Gyuwon Jung <152982748+Q1-in-Dublin@users.noreply.github.com>
2026-09-09 14:45:26 +0100

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,39 @@
+from collections import deque
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # n seems like the number of nodes
+        # list of undirected , could be circular? no circular
+        # output t/f
+        # what is valid tree?
+        # Tree => no circular ,every node should be connected
+        # n 개 node's edge is always n-1 
+        if len(edges) != n-1:
+            return False
+        # {0: [1, 2, 3], 1: [0, 4], 2: [0], 3: [0], 4: [1]}
+        adj = {i : [] for i in range(n)}
+        #print(adj)
+        for node1, node2 in edges:
+            adj[node1].append(node2)
+            adj[node2].append(node1)
+
+        visited = set([0]) #need once
+        queue = deque([0])
+
+        while queue:
+            node = queue.popleft()
+            
+            for neighbor in adj[node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+        return len(visited) == n
+            #visit + queue
+            
+            #consumming que and adding
+
+
+        #time complexity : O(N+E) => O(n)
+        # space : adj v, u  , Visited , queue
+        #O(V+E)
+
+        
