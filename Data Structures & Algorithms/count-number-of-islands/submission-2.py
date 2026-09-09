1bd3e097d02ceb3a97b76661b358bf7efbf87dfe
Jacob Gyuwon Jung <152982748+Q1-in-Dublin@users.noreply.github.com>
2026-09-09 21:34:29 +0100

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -0,0 +1,47 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # island is adjacent lands horizontally or vertically and surrounded by 0
+        #check (-1,0) , (1,0) (0,-1) , (0,1)
+        #                                0
+        # island is connecting adjacent 010
+                                        #0
+        count = 0 # number of island
+        seen = set()
+        rows,cols = len(grid), len(grid[0])
+        
+        def dfs(i,j):
+            if (i<0 or i>= len(grid)) or j<0 or j>=len(grid[0]) or grid[i][j] =='0' or (i,j) in seen:
+                return
+            seen.add((i,j))
+            dfs(i-1,j)
+            dfs(i+1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
+        
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] =='1' and (i,j) not in seen:
+                    count+=1 
+                    dfs(i,j)
+        return count
+        # def dfs(i,j):
+        #     if (i<0 or i >= len(grid)) or (j<0 or j>=len(grid[0])) or (i,j) in seen or grid[i][j] == '0':
+        #         return
+
+        #     seen.add((i,j))
+        #     dfs(i-1,j)
+        #     dfs(i+1,j)
+        #     dfs(i,j-1)
+        #     dfs(i,j+1)
+
+        # count = 0
+        # seen = set()
+        # for i in range(len(grid)) : 
+        #     for j in range(len(grid[0])):
+        #         if grid[i][j] == '1' and (i,j) not in seen:
+        #             count +=1
+        #             dfs(i,j)
+
+    
+        # return count
